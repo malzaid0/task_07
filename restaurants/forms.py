9d2784852fb80f59a2825e8432424fe34e241dbf
Mohammed Alzaid <m.al-zaid@live.com>
2020-09-01 11:55:05 +0300

    added a base file

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -5,3 +5,7 @@ class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
         fields = '__all__'
+
+        # widgets = {
+        #     'publish_date': forms.DateInput(attrs={"type": "date"})
+        # }
